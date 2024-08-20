9806ad1533c214ad60c51cf05d5f5654338ab685
Nikolay-str <55555mett@gmail.com>
2024-08-20 19:44:52 +0300

    New
    Получил входным параметром название города и передал это название в метода process_data

--- UI/console_interface.py
+++ UI/console_interface.py
@@ -11,5 +11,17 @@ class ConsoleInterface:
 
     def __data_screen(self, name_town):
         if name_town:
+            self.name_town = name_town
+        return name_town
 
 
+
+    def __process_data(self, name_town: str):
+        self.web_client = OWM('https://api.openweathermap.org', settings.API_KEY)
+        coords = self.web_client.get_city_coords(name_town)
+        json_forecast = self.web_client.get_city_forecast(coords)
+
+        result = GeneralClass()
+        result.model_validate(json_forecast)
+
+        return result
