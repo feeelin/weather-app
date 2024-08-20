c0d069bbe7bc54b84b1be4ebfda9838aec887f08
morphboxing89 <phenomen0m@mail.ru>
2024-08-20 22:49:35 +0300

    changes

--- serialization/serialize_wind.py
+++ serialization/serialize_wind.py
@@ -1,21 +1,7 @@
 from pydantic import BaseModel
-import json
-
-
-def read_input_json(filename: str) -> dict:
-    with open(file=filename, encoding="UTF-8") as file:
-        output = json.load(file)
-    return output
 
 
 class Wind(BaseModel):
-    wind: dict
-
-    def get_wind(self):
-        return self.wind
-
-
-if __name__ == "__main__":
-    example_data = read_input_json('data.json')
-    wind = Wind(**example_data)
-    print(wind)
+    speed: float or int
+    deg: float or int
+    gust: float or int
---
+++ tests/serialization/test_serialize_wind.py
@@ -0,0 +1,18 @@
+import unittest
+from serialize_wind import Wind
+
+
+class TestSerializeWind(unittest.TestCase):
+    def test_wind(self):
+        test_obj = {
+            "speed": 0.62,
+            "deg": 349,
+            "gust": 1.18
+        }
+        test_wind = Wind(speed=0.62, deg=349, gust=1.18)
+        test_wind_2 = Wind.model_validate(test_obj)
+        self.assertEqual(test_wind == test_wind_2, True)
+
+
+if __name__ == '__main__':
+    unittest()
