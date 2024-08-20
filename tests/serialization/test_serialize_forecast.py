f7a061059dba02059e3e6afa3a6b0524b7fb68e0
Goodchild21 <Goodchild_21@mail.ru>
2024-08-20 20:01:27 +0300

    jfdshj

---
+++ tests/serialization/test_serialize_forecast.py
@@ -0,0 +1,20 @@
+import unittest
+from serialize_forecast import Weather
+
+
+class TestSerializeForecast(unittest.TestCase):
+    def test_weather(self):
+        test_obj = {
+            "id": 501,
+            "main": "Rain",
+            "description": "moderate rain",
+            "icon": "10d"
+        }
+        test_weather = Weather(id=501,main="Rain",description="moderate rain",icon="10d")
+        test_weather_2 = Weather.model_validate(test_obj)
+        # test_weather_2.model_validate(test_obj)
+        self.assertEqual(test_weather == test_weather_2, True)
+
+
+if __name__ == '__main__':
+    unittest()
