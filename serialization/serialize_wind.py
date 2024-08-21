670ad003c9e5e73044c5f3bec26f695f2954d3d7
morphboxing89 <phenomen0m@mail.ru>
2024-08-21 12:27:43 +0300

    qqqq

--- serialization/serialize_wind.py
+++ serialization/serialize_wind.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class Wind(BaseModel):
-    speed: float or int
-    deg: float or int
-    gust: float or int
+    speed: float
+    deg: int
+    gust: float
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import unittest
 from tests.web.test_owm_http_client import TestOpenWeatherMapHttpClient
 from tests.serialization.test_serialize_forecast import TestSerializeForecast
-from tests.serialization.test_serialize_wind import TestSerializeWind
+from tests.serialization_wind.test_serialize_wind import TestWind
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(TestOpenWeatherMapHttpClient())
     suite.addTest(TestSerializeForecast())
-    suite.addTest(TestSerializeWind())
+    suite.addTest(TestWind())
     unittest.main()
