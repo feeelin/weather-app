48fccc28cf53d5a584c1ed8de416ae7a550c1d61
Valerii Filatov <115320882+feeelin@users.noreply.github.com>
2024-08-21 12:48:37 +0300

    Main tests update
    Обновил точку входа для тестов с учётом последних правок в сценарий тестирования сериализации информации о ветре

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import unittest
 from tests.web.test_owm_http_client import TestOpenWeatherMapHttpClient
 from tests.serialization.test_serialize_forecast import TestSerializeForecast
-from tests.serialization_wind.test_serialize_wind import TestWind
+from tests.serialization.test_serialize_wind import TestSerializeWind
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(TestOpenWeatherMapHttpClient())
     suite.addTest(TestSerializeForecast())
-    suite.addTest(TestWind())
+    suite.addTest(TestSerializeWind())
     unittest.main()
