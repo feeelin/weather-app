80bd6b682b4ec934c4601f2b0d8642cf14cfa211
Valery Filatov <valiklfiatoff@yandex.ru>
2024-08-22 23:26:13 +0300

    Fixing tests
    Пофиксил падение тестов после прошлого обновления

--- tests/web/test_owm_http_client.py
+++ tests/web/test_owm_http_client.py
@@ -9,7 +9,7 @@ class TestOpenWeatherMapHttpClient(unittest.TestCase):
 
     def test_city_response(self):
         result = self.client.get_city_coords('Tula')
-        self.assertEqual(type(result), list)
+        self.assertEqual(type(result), dict)
 
     def test_forecast_response(self):
         result = self.client.get_city_forecast([{"lat": 15, "lon": 15}])
